a7801751f3fc57634031e7b440de316f54eaf7db
nayeonkinn <98254573+nayeonkinn@users.noreply.github.com>
2022-06-09 17:02:07 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -5,17 +5,13 @@ int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	string result = "EABCD";
 	for (int i = 0; i < 3; i++) {
 		int a, cnt = 0;
 		for (int j = 0; j < 4; j++) {
 			cin >> a;
 			if (a == 0) cnt++;
 		}
-		if (cnt == 1) cout << 'A';
-		else if (cnt == 2) cout << 'B';
-		else if (cnt == 3) cout << 'C';
-		else if (cnt == 4) cout << 'D';
-		else cout << 'E';
-		cout << '\n';
+		cout << result[cnt] << '\n';
 	}
 }
